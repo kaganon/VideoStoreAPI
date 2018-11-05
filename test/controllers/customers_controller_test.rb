a6977521b213c81fe1bf4412f6970352f1c4de35
Amber Lynn <griffiam@gmail.com>
2018-11-05 14:57:33 -0800

    customer model testing

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,7 +1,111 @@
 require "test_helper"
 
 describe CustomersController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
+  # describe "index" do
+  # it "should get index" do
+  #   # Act
+  #   get customers_path
+  #
+  #   # Assert
+  #   must_respond_with :success
+  # end
+  ############################
+  #   describe "create" do
+  # it "can create a customer with valid data" do
+  #   # Arrange
+  #   customer_data = {
+  #     customer: {
+  #       name: "new test customer",
+  #       author_id: Author.first.id
+  #     }
+  #   }
+  #
+  #   # Assumptions
+  #   test_customer = Customer.new(customer_data[:customer])
+  #   test_customer.must_be :valid?, "Customer data was invalid. Please come fix this test"
+  #
+  #   # Act
+  #   expect {
+  #     post customers_path, params: customer_data
+  #   }.must_change('Customer.count', +1)
+  #
+  #   # Assert
+  #   must_redirect_to customer_path(Customer.last)
+  # end
+  #
+  # it "does not create a new customer w/ invalid data" do
+  #     # Arrange
+  #     customer_data = {
+  #       customer: {
+  #         title: Customer.first.title,
+  #         author_id: Author.first.id
+  #       }
+  #     }
+  #
+  #     # Assumptions
+  #     Customer.new(customer_data[:customer]).wont_be :valid?, "Customer data wasn't invalid. Please come fix this test"
+  #
+  #     # Act
+  #     expect {
+  #       post customers_path, params: customer_data
+  #     }.wont_change('Customer.count')
+  #
+  #     # Assert
+  #     must_respond_with :bad_request
+  #   end
+  # end
+  ###############################
+  # describe "show" do
+  #
+  #    it "should respond with success for showing an existing customer" do
+  #      # Arrange
+  #      existing_customer = customers(:poodr)
+  #
+  #      # Act
+  #      get customer_path(existing_customer.id)
+  #
+  #      # Assert
+  #      must_respond_with :success
+  #    end
+  #
+  #    it "should respond with not found for showing a non-existing customer" do
+  #      # Arrange
+  #      customer = customers(:poodr)
+  #      id = customer.id
+  #
+  #      get customer_path(id)
+  #      must_respond_with :success
+  #
+  #      customer.destroy
+  #
+  #      # Act
+  #      get customer_path(id)
+  #
+  #      # Assert
+  #      must_respond_with :missing
+  #    end
+  #
+  #  end
+  ######################################
+  # describe "destroy" do
+  #   it "can destroy an existing customer" do
+  #     # Arrange
+  #     customer = customers(:poodr)
+  #     # before_customer_count = Customer.count
+  #
+  #     # Act
+  #     expect {
+  #       delete customer_path(customer)
+  #     }.must_change('Book.count', -1)
+  #
+  #     # Assert
+  #     must_respond_with :redirect
+  #     must_redirect_to customers_path
+  #
+  #     # expect(Customer.count).must_equal(
+  #     #   before_customer_count - 1,
+  #     #   "customer count did not decrease"
+  #     # )
+  #   end
   # end
 end
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,9 +1,106 @@
 require "test_helper"
 
 describe Customer do
-  let(:customer) { Customer.new }
+  # VALIDATIONS
+  describe 'validations' do
+    let(:customer) { Customer.create(
+      name: "Mickey"
+      registered_at: Date.parse('2020-11-01'),
+      address: '1234 W. 5th Street',
+      city: "Avondale",
+      state: "Arizona",
+      postal_code: "90210",
+      phone: "555-867-5309"
+    )
+    }
 
-  it "must be valid" do
-    value(customer).must_be :valid?
+    it 'is invalid without a name' do
+      customer.name = nil
+
+      customer.name.must_be_nil
+      expect(customer).must_be :invalid?
+    end
+
+    it 'is invalid without a registration date' do
+      customer.registered_at = nil
+
+      customer.registered_at.must_be_nil
+      expect(customer).must_be :invalid?
+    end
+
+    it 'is invalid without an address' do
+      customer.address = nil
+
+      customer.address.must_be_nil
+      expect(customer).must_be :invalid?
+    end
+
+    it 'is invalid without a city' do
+      customer.city = nil
+
+      customer.city.must_be_nil
+      expect(customer).must_be :invalid?
+    end
+
+    it 'is invalid without a state' do
+      customer.state = nil
+
+      customer.state.must_be_nil
+      expect(customer).must_be :invalid?
+    end
+
+    it 'is invalid without a postal_code' do
+      customer.postal_code = nil
+
+      customer.postal_code.must_be_nil
+      expect(customer).must_be :invalid?
+    end
+
+    it 'is invalid without a phone number' do
+      customer.phone = nil
+
+      customer.phone.must_be_nil
+      expect(customer).must_be :invalid?
+    end
+
+    it 'is valid with all fields present' do
+      expect(customer).must_be :valid?
+    end
+
+
+  end
+
+
+  # RELATIONS
+  describe 'Relations' do
+    let(:customer) { Customer.create(
+      name: "Mickey"
+      registered_at: Date.parse('2020-11-01'),
+      address: '1234 W. 5th Street',
+      city: "Avondale",
+      state: "Arizona",
+      postal_code: "90210",
+      phone: "555-867-5309"
+    )
+    }
+
+    let(:movie_1) { Movie.create(
+      title: 'Five Nights at Freddys',
+      release_date: Date.parse('1994-07-12'),
+      overview: 'Scariness on film reel',
+      inventory: 5
+      )
+    }
+
+    it 'must relate to a customer' do
+      customer.movies << movie_1
+
+      expect(customer).must_respond_to :movies
+      expect(customer.movies.first).must_equal movie_1
+    end
+
+    # it 'can set the customers' do
+    # end
+
   end
 end
