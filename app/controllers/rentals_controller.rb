36b106693d70073a832b7c24b0245a2ee2616c37
Amber Lynn <griffiam@gmail.com>
2018-11-07 10:10:37 -0800

    made additions to checkin and checkout rental methods

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -7,26 +7,12 @@ class RentalsController < ApplicationController
       date = Date.today
       rental.checkout_date = date
       rental.due_date = (date + 7)
-      movie = Movie.find_by(id: rental.movie_id)
-      rental.movie_inventory(movie)
+      movie = rental.movie
+      movie.available_inventory - 1
 
-    # check inventory before save
-    movie = Movie.find_by(id: :movie_id)
-
-    if movie.available_inventory > 0
-
-      if rental.save
-        movie.available_inventory - 1
-      else
-        render_error(:not_found, rental.errors.messages)
-      end
-    else
-      render_error(:bad_request)
-    end
-      render json: { customer_id: :customer_id}
+      render json: { customer_id: :customer_id }
 
     end
-
   end
 
   # This is a POST verb
@@ -38,6 +24,4 @@ class RentalsController < ApplicationController
   def rental_params
     params.permit(:customer_id, :movie_id)
   end
-
-
 end
