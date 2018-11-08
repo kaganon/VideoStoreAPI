7f439a4923aff5c308ed716b5ffa30f17aa1516b
Katrina Aganon <katrina.aganon@gmail.com>
2018-11-08 09:40:01 -0800

    update customer find_movie method

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,28 +1,15 @@
 class Customer < ApplicationRecord
   validates :name, :registered_at, :address, :city, :state, :postal_code, :phone, presence: true
-  has_many :rentals
+  has_many :rentals, dependent: :destroy
 
   def find_rental(movie)
-    if self.rentals
-      self.rentals.each do |rental|
-        return rental if rental.movie == movie
-      else return ArgumentError
-      end
+    self.rentals.each do |rental|
+      return rental if rental.movie == movie
     end
-
   end
 
   def movies_checked_out_count
-<<<<<<< HEAD
     return self.rentals.where(checkin_date: nil).count
-=======
-    checked_out =
-    self.rentals.empty? ? 0 : self.rentals.where.not(checkout_date: nil).count
-
-    checked_in = self.rentals.empty? ? 0 : self.rentals.where.not(checkin_date: nil).count
-
-    return checked_out - checked_in
->>>>>>> 04914cfac7d2c42b18f1e095bf762fb28aa7f563
   end
 
 
